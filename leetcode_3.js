4e5614adc47b5b9570c2548c62ebeaf5004f66cd
niemoBroIntegration <49417819+niemoBroIntegration@users.noreply.github.com>
2022-07-28 02:02:56 -0400

    jfc

--- leetcode_3.js
+++ leetcode_3.js
@@ -1,35 +1,49 @@
 function lengthOfLongestSubstring(s) {
+    var length = s.length;
+    if (length === 0) {
+        return 0;
+    }
+    if (length === 1) {
+        return 1;
+    }
+    var sub = "";
+    var start = 0;
     var longest = 0;
-    var curr = "";
-    for (var start = 0; start < s.length - 1; start++) {
-        for (var end = start + 1; end < s.length; end++) {
-            curr = s.slice(start, end);
-            if (isVailidSubstring(curr) && curr.length > longest) {
-                longest = curr.length;
-            }
+    var end = 1;
+    sub = s[start] + s[end];
+    while (end < length) {
+        console.log("SUB", start, end, sub);
+        if (sub.includes(s[end + 1])) {
+            console.log("RESET");
+            start = end + 1;
+            end += 2;
+            sub = s[start] + s[end];
         }
+        else {
+            sub += s[end];
+            end++;
+        }
+        longest = end - start + 1 > longest ? end - start + 1 : longest;
     }
     return longest;
 }
-var isVailidSubstring = function (s) {
-    for (var i = 0; i < s.length - 1; i++) {
-        for (var j = i + 1; j < s.length; j++) {
-            if (s[i] === s[j]) {
-                return false;
-            }
-        }
-    }
-    return true;
-};
 var s = [
-    { string: "abcabcbb", expected: 3, output: 3 },
-    { string: "pwwkew", expected: 3, output: 3 },
-    { string: " ", expected: 1, output: 0 },
-    { string: "au", expected: 2, output: 1 },
-    { string: "bbbbb", expected: 1, output: 1 },
-    { string: "bwf", expected: 3, output: 2 },
+    { string: "abcabcbb", expected: 3 },
+    { string: "pwwkew", expected: 3 },
+    { string: " ", expected: 1 },
+    { string: "au", expected: 2 },
+    { string: "bbbbb", expected: 1 },
+    { string: "bwf", expected: 3 },
+    { string: "bwwfxx", expected: 3 },
+    { string: "bwwfxxasdff", expected: 5 },
+    { string: "abcdefg", expected: 5 },
 ];
 s.forEach(function (x, i) {
-    console.log("exp:", x.expected, "fn:", lengthOfLongestSubstring(x.string), "out", x.output);
+    // console.log(x.string, isVailidSubstring(x.string));
+    var length = lengthOfLongestSubstring(x.string);
+    console.log("exp:", x.expected, "fn:", length, "|", "'".concat(x.string, "'"));
+    if (x.expected !== length) {
+        console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+    }
     console.log("________________________________________");
 });
--- leetcode_3.ts
+++ leetcode_3.ts
@@ -1,51 +1,57 @@
 function lengthOfLongestSubstring(s: string): number {
-    let longest = 0;
-    let curr = "";
-    for (let start = 0; start < s.length - 1; start++) {
-        for (let end = start + 1; end < s.length; end++) {
-            curr = s.slice(start, end);
-            if (isVailidSubstring(curr) && curr.length > longest) {
-                longest = curr.length;
-            }
-        }
-    }
+    let length = s.length;
 
+    if (length === 0) {
+        return 0;
+    }
+    if (length === 1) {
+        return 1;
+    }
+    let sub: string = "";
+    let start: number = 0;
+    let longest: number = 0;
+    let end: number = 1;
+    sub = s[start] + s[end];
+    while (end < length) {
+        console.log("SUB", start, end, sub);
+        if (sub.includes(s[end + 1])) {
+            console.log("RESET");
+            start = end + 1;
+            end += 2;
+            sub = s[start] + s[end];
+        } else {
+            sub += s[end];
+            end++;
+        }
+        longest = end - start + 1 > longest ? end - start + 1 : longest;
+    }
     return longest;
 }
-
-const isVailidSubstring = (s: string): boolean => {
-    for (let i = 0; i < s.length - 1; i++) {
-        for (let j = i + 1; j < s.length; j++) {
-            if (s[i] === s[j]) {
-                return false;
-            }
-        }
-    }
-    return true;
-};
 
 interface e {
     string: string;
     expected: number;
-    output: number;
 }
 const s: e[] = [
-    { string: "abcabcbb", expected: 3, output: 3 },
-    { string: "pwwkew", expected: 3, output: 3 },
-    { string: " ", expected: 1, output: 0 },
-    { string: "au", expected: 2, output: 1 },
-    { string: "bbbbb", expected: 1, output: 1 },
-    { string: "bwf", expected: 3, output: 2 },
+    { string: "abcabcbb", expected: 3 },
+    { string: "pwwkew", expected: 3 },
+    { string: " ", expected: 1 },
+    { string: "au", expected: 2 },
+    { string: "bbbbb", expected: 1 },
+    { string: "bwf", expected: 3 },
+    { string: "bwwfxx", expected: 3 },
+    { string: "bwwfxxasdff", expected: 5 },
+    { string: "abcdefg", expected: 5 },
 ];
 
 s.forEach((x, i) => {
-    console.log(
-        "exp:",
-        x.expected,
-        "fn:",
-        lengthOfLongestSubstring(x.string),
-        "out",
-        x.output
-    );
+    // console.log(x.string, isVailidSubstring(x.string));
+    let length = lengthOfLongestSubstring(x.string);
+    console.log("exp:", x.expected, "fn:", length, "|", `'${x.string}'`);
+    if (x.expected !== length) {
+        console.log(
+            "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+        );
+    }
     console.log("________________________________________");
 });
